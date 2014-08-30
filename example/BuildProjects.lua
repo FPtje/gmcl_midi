88c393ab363d78dc3f046f97c27d5780059ffe3f
FPtje <fpeijnenburg@gmail.com>
2014-08-30 13:31:02 +0200

    Initial modifications

--- example/BuildProjects.lua
+++ example/BuildProjects.lua
@@ -1,22 +1,22 @@
-solution "gmsv_example"
+solution "gmcl_midi"
 
 	language "C++"
 	location ( os.get() .."-".. _ACTION )
 	flags { "Symbols", "NoEditAndContinue", "NoPCH", "StaticRuntime", "EnableSSE" }
 	targetdir ( "lib/" .. os.get() .. "/" )
 	includedirs { "../include/" }
-	
+
 	configurations
-	{ 
+	{
 		"Release"
 	}
-	
+
 	configuration "Release"
 		defines { "NDEBUG" }
 		flags{ "Optimize", "FloatFast" }
-	
-	project "gmsv_example"
+
+	project "gmcl_midi"
 		defines { "GMMODULE" }
 		files { "src/**.*", "../include/**.*" }
 		kind "SharedLib"
-		
+
